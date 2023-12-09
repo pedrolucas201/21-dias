6de13b7395d8a11ffd83db51ab1afb2376d4b591
Pedro Lucas <quizueiramusj@gmail.com>
2023-12-08 22:49:05 -0300

    Novos aprendizados em JS, usando console.

--- dia03/script.js
+++ dia03/script.js
@@ -1,2 +1,9 @@
-let entradaDoUsuario = prompt();
-console.log(entradaDoUsuario);
+let nome = prompt('Digite seu nome: ');
+let idade = parseInt(prompt('Digite sua idade: '));
+let altura = parseFloat(prompt('Digite sua altura: '));
+let peso = parseFloat(prompt('Digite seu peso: '));
+
+let anoNasc = (2023 - idade);
+let IMC = peso/(altura * altura);
+
+console.log('Olá ' + nome + ', você tem ' + idade + ' anos, nasceu em ' + anoNasc + ', tem ' + altura + ' de altura' + ', pesa ' + peso + ' e seu IMC é de: ' + IMC);
